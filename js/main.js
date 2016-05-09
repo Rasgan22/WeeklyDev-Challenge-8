bddd923e1cce5f70f14a900202d92317714a6081
Rasgan22 <plewicki01@gmail.com>
2016-05-09 18:21:37 +0200

    arrow animation, bars on scroll

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ var circle = new ProgressBar.Circle('#projects', {
     trailWidth: 1,
     duration: 1500,
     text: {
-        value: 'Big projects'
+        value: '0'
     },
     step: function (state, bar) {
         bar.setText((bar.value() * 100).toFixed(0));
     }
 });
-circle.animate(0.5);
-var circle = new ProgressBar.Circle('#clients', {
+
+var circle1 = new ProgressBar.Circle('#clients', {
     color: '#009989',
     strokeWidth: 2,
     trailWidth: 1,
@@ -23,8 +23,8 @@ var circle = new ProgressBar.Circle('#clients', {
         bar.setText((bar.value() * 100).toFixed(0));
     }
 });
-circle.animate(0.35);
-var circle = new ProgressBar.Circle('#months', {
+
+var circle2 = new ProgressBar.Circle('#months', {
     color: '#152b3c',
     strokeWidth: 2,
     trailWidth: 1,
@@ -36,4 +36,25 @@ var circle = new ProgressBar.Circle('#months', {
         bar.setText((bar.value() * 100).toFixed(0));
     }
 });
-circle.animate(0.8);
+
+$(document).ready(function () {
+	$(document).scroll(function() {
+		var y = $(this).scrollTop() + 300;
+		var x = $('#numbers').position();
+		if (y > x.top) {
+			circle.animate(0.5);
+			circle2.animate(0.8);
+			circle1.animate(0.35);	
+		};
+	});
+	function loop() {
+		$('#triangle-bottom').css('top', '70%');
+		$('#triangle-bottom').animate({
+			top: '80%',
+		}, 1000, function (){
+			loop();
+		});
+	}
+	loop();
+});
+
